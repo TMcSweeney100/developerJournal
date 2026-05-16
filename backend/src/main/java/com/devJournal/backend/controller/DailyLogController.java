3be63b91090a026d61c7f28e53773f9f95403670
tim <timmcsweeney12@hotmail.com>
2026-05-16 08:56:05 +0100

    added controller for daily logs

---
+++ backend/src/main/java/com/devJournal/backend/controller/DailyLogController.java
@@ -0,0 +1,44 @@
+package com.devJournal.backend.controller;
+
+import com.devJournal.backend.entity.DailyLog;
+import com.devJournal.backend.service.DailyLogService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping
+public class DailyLogController {
+    private final DailyLogService dailyLogService;
+
+    public DailyLogController(DailyLogService dailyLogService) {
+        this.dailyLogService = dailyLogService;
+    }
+
+    @GetMapping
+    public List<DailyLog> getAllDailyLogs(){
+        return dailyLogService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<DailyLog> getDailyLogById(@PathVariable Long id){
+        return dailyLogService.findById(id);
+    }
+
+    @PostMapping()
+    public DailyLog saveDailyLog(@RequestBody DailyLog dailyLog){
+        return dailyLogService.save(dailyLog);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDailyLogById(@PathVariable Long id){
+        dailyLogService.deleteById(id);
+    }
+
+    
+
+
+
+
+}
